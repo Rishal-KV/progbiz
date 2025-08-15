ba7b209c3790ebc1e59e944eb1afd94f3ad6ee23
Rishal-KV <rishkv923@gmail.com>
2025-08-16 03:22:31 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,12 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["gratisography.com","res.cloudinary.com"],
   },
-
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
