2ccdc98f0d6f0fec4cc93bc06d5dd597f7b9a2c3
Rishal-KV <rishkv923@gmail.com>
2025-08-16 12:47:45 +0530

    first commit

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
+    withCredentials: true,
     baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api/v1/user/landing`,
     headers: {
         "Content-Type": "application/json",
+        
         'Access-Control-Allow-Origin': 'true' // incorrect
 
     },
